e52b58ba37e9de801939d28323d22412b38c6a97
nithin-code-web <nithinbudime@gmail.com>
2026-04-21 16:43:39 +0530

    solved leap year

---
+++ phase-1-conditions/level-1/06_larger_of_two.py
@@ -0,0 +1,22 @@
+# Problem:
+# Find and print the larger of two numbers
+
+# Input:
+# Two integer numbers
+
+# Output:
+# Print the larger number
+
+# Logic:
+num1 = int(input("enter a number :"))
+num2 = int(input("enter a number :"))
+if num1 > num2:
+    print('num1 is greater')
+else:
+    print('num2 is greater')
+
+# Time Complexity:
+
+
+# Space Complexity:
+
